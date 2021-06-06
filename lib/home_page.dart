a814f931ffcc579b481003aa181d77de891fded3
Satyam Kaurav <satyamkaurav.sk7@gmail.com>
2021-06-06 17:04:38 +0530

    DAY 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final int days = 30;
+  final String name = "Codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
