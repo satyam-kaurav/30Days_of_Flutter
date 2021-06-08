cda7ccce36af165dcbc051a1483734caab4d55f6
Satyam Kaurav <satyamkaurav.sk7@gmail.com>
2021-06-08 21:58:19 +0530

    DAY10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,11 @@
+class Item {
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      this.id, this.name, this.description, this.price, this.color, this.image);
+}
