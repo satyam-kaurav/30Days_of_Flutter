de6a620575a013ea8a8517fb8b0e530c73db5f8c
Satyam Kaurav <satyamkaurav.sk7@gmail.com>
2021-06-13 20:58:15 +0530

    DAY22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -18,6 +17,65 @@ class CartPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            "\$9999".text.xl5.color(context.accentColor).make(),
+            30.widthBox,
+            ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(context.theme.buttonColor),
+                    ),
+                    child: "Buy".text.color(context.accentColor).make())
+                .w32(context)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.add_circle),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle),
+          onPressed: () {},
+        ),
+        title: "item1".text.make(),
       ),
     );
   }
